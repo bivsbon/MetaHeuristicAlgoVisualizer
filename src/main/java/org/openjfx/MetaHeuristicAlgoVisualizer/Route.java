ce82cf6da4b4f40cb1f617a4b7600e57e54e3efd
Ming <thanhminh20002005@gmail.com>
2021-05-24 13:32:46 +0700

    implements brute force solution for testing

--- src/main/java/org/openjfx/MetaHeuristicAlgoVisualizer/Route.java
+++ src/main/java/org/openjfx/MetaHeuristicAlgoVisualizer/Route.java
@@ -5,29 +5,45 @@ import java.util.ArrayList;
 import javafx.geometry.Point2D;
 
 public class Route {
-	private ArrayList<Point2D> cityOrder;
+	private final ArrayList<Point2D> cityList;
+	private ArrayList<Integer> order = new ArrayList<>();
 	
-	public ArrayList<Point2D> getCityOrder() {
-		return cityOrder;
+	public ArrayList<Integer> getRoute() {
+		return order;
 	}
 	
 	public void swapCity(int index1, int index2) {
 		//TODO: Swap the order of 2 city in route
 	}
 	
-	public Route(ArrayList<Point2D> cityOrder) {
-		this.cityOrder = cityOrder;
+	public Route(ArrayList<Point2D> cityList) {
+		this.cityList = cityList;
+		for (int i = 0; i < cityList.size(); i++) {
+			order.add(i);
+		}
+	}
+
+	public ArrayList<Integer> getOrder() {
+		return order;
+	}
+
+	public void setOrder(ArrayList<Integer> order) {
+		this.order = order;
 	}
 	
 	public double getTotalDistance() {
 		double totalDistance = 0.0;
 		
-		for (int i = 1; i < cityOrder.size(); i++) {
+		for (int i = 1; i < order.size(); i++) {
 			// Add the distance of current city to previous city
-			totalDistance += cityOrder.get(i).distance(cityOrder.get(i-1));
+			Point2D city1 = cityList.get(order.get(i));
+			Point2D city2 = cityList.get(order.get(i-1));
+			totalDistance += city1.distance(city2);
 		}
 		// Add the distance of the last city to first city
-		totalDistance += cityOrder.get(0).distance(cityOrder.get(cityOrder.size()-1));
+		Point2D firstCity = cityList.get(order.get(0));
+		Point2D lastCity = cityList.get(order.get(order.size()-1));
+		totalDistance += firstCity.distance(lastCity);
 		return totalDistance;
 	}
 }
--- src/main/java/test/algo/AlgoTest.java
+++ src/main/java/test/algo/AlgoTest.java
@@ -1,9 +1,51 @@
 package test.algo;
 
+import java.util.ArrayList;
+import org.openjfx.MetaHeuristicAlgoVisualizer.Route;
+
+import javafx.geometry.Point2D;
+
 public class AlgoTest {
+	private static final int N = 10;
+	private static double shortest = Double.MAX_VALUE;
+	static ArrayList<Point2D> cities = new ArrayList<Point2D>();
+	static ArrayList<Integer> order = new ArrayList<>();
+	static boolean[] chosen = new boolean[N];
 
 	public static void main(String[] args) {
+		cities.add(new Point2D(5.8, 0.2));
+		cities.add(new Point2D(1.0, 9.2));
+		cities.add(new Point2D(9.8, 2.5));
+		cities.add(new Point2D(5.3, 7.0));
+		cities.add(new Point2D(3.7, 6.3));
+		cities.add(new Point2D(0.8, 9.5));
+		cities.add(new Point2D(6.2, 7.6));
+		cities.add(new Point2D(8.4, 8.2));
+		cities.add(new Point2D(9.7, 1.2));
+		cities.add(new Point2D(5.7, 5.7));
+		Route route = new Route(cities);
+		search(route);
 		
+		System.out.println(shortest);
 	}
 
+	private static void search(Route route) {
+		if (order.size() == N) {
+			route.setOrder(order);
+			double currentCost = route.getTotalDistance();
+			if (shortest > currentCost) {
+				shortest = currentCost;
+			}
+		}
+		else {
+			for (int i = 0; i < N; i++) {
+				if (chosen[i]) continue;
+				chosen[i] = true;
+				order.add(i);
+				search(route);
+				order.remove(order.size()-1);
+				chosen[i] = false;
+			}
+		}
+	}
 }
