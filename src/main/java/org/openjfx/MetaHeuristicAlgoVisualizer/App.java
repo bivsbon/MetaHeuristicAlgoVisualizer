b318a2bd5c58c7c49ff9639df7f03b15cc6dc9b4
Ming <thanhminh20002005@gmail.com>
2021-06-03 01:48:20 +0700

    Geting ready for executable

--- src/main/java/org/openjfx/MetaHeuristicAlgoVisualizer/App.java
+++ src/main/java/org/openjfx/MetaHeuristicAlgoVisualizer/App.java
@@ -17,18 +17,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 1280, 720);
+//    	Parent root = FXMLLoader.load(getClass().getResource("/resources/org/openjfx/MetaHeuristicAlgoVisualizer/primary.fxml"));
+    	Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+        scene = new Scene(root, 1280, 720);
         stage.setScene(scene);
         stage.show();
-    }
-    
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
